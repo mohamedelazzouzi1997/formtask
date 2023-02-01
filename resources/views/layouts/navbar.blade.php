65794b67e0a4c803571296d257b3a11babf48180
mohamed el azzouzi <med.elazzouzi.97@gmail.com>
2023-02-01 15:02:06 +0100

    add style to navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,5 @@
-	<header id="navtop" class="">
+
+    <header id="navtop" class="">
 		{{-- <div class="menu-container"> --}}
 			<div class="grt-menu-row items-center h-[100px]">
 				<div data-aos="fade-down" class="grt-menu-logo">
@@ -8,10 +9,10 @@
 				<nav class="flex z-50">
 				 <button class="grt-mobile-button"><span class="line1"></span><span class="line2"></span><span class="line3"></span></button>
 				 <ul class="grt-menu">
-				  <li class=""><a href="{{ url('/') }}">HOME</a></li>
-				  <li><a href="{{ url('/#about') }}">ABOUT</a></li>
-				  <li><a href="{{ url('/#reservation') }}">RESERVATION</a></li>
-				<li><a href="{{ url('/contact') }}">CONTACT</a></li>
+				  <li class=""><a class="@if (Request::path() == '/') border-b border-orange-300 @endif" href="{{ url('/') }}">HOME</a></li>
+				  {{-- <li><a class="@if (Request::path() == '/') border-b border-orange-300 @endif" href="{{ url('/#about') }}">ABOUT</a></li> --}}
+				  {{-- <li><a class="@if (Request::path() == '/') border-b border-orange-300 @endif" href="{{ url('/#reservation') }}">RESERVATION</a></li> --}}
+				<li><a class="@if (Request::path() == 'contact') border-b border-orange-300 @endif" href="{{ url('/contact') }}">CONTACT</a></li>
 				</ul>
 				</nav>
 				</div>
